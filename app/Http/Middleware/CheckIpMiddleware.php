2952f2862ee06b5804b16ee80284895a4c7d974e
Roger Wu (吳偉召) <roger@icarry.me>
2024-11-06 15:01:05 +0800

    關閉IP檢查

--- app/Http/Middleware/CheckIpMiddleware.php
+++ app/Http/Middleware/CheckIpMiddleware.php
@@ -18,36 +18,36 @@ class CheckIpMiddleware
      */
     public function handle($request, Closure $next)
     {
-        $whiteIps = IpAddressDB::where('is_on',1)->select('ip')->get()->pluck('ip')->all();
-        $systemSetting = SystemSettingDB::first();
-        !empty($systemSetting->disable_ip_start) ? $startTime = $systemSetting->disable_ip_start : $startTime = null;
-        !empty($systemSetting->disable_ip_end) ? $endTime = $systemSetting->disable_ip_end : $endTime = null;
+        // $whiteIps = IpAddressDB::where('is_on',1)->select('ip')->get()->pluck('ip')->all();
+        // $systemSetting = SystemSettingDB::first();
+        // !empty($systemSetting->disable_ip_start) ? $startTime = $systemSetting->disable_ip_start : $startTime = null;
+        // !empty($systemSetting->disable_ip_end) ? $endTime = $systemSetting->disable_ip_end : $endTime = null;
 
-        if(strtotime($startTime) <= strtotime(date('Y-m-d')) && strtotime(date('Y-m-d')) <= strtotime($endTime)){
-            if (auth()->user() && in_array(auth()->id(), [40])) {
-                \Debugbar::enable();
-            }
-            else {
-                \Debugbar::disable();
-            }
-        }else{
-            if(!empty($request->header('x-forwarded-for'))){
-                $ip = $request->header('x-forwarded-for');
-            }else{
-                $ip = $request->ip();
-            }
-            if (!in_array($ip, $whiteIps)) {
-                return response("your ip address ( $ip ) is not valid.")->header('Content-Type', 'text/plain');
-                // return redirect()->to('https://google.com');
-            }else{
-                if (auth()->user() && in_array(auth()->id(), [40])) {
-                    \Debugbar::enable();
-                }
-                else {
-                    \Debugbar::disable();
-                }
-            }
-        }
+        // if(strtotime($startTime) <= strtotime(date('Y-m-d')) && strtotime(date('Y-m-d')) <= strtotime($endTime)){
+        //     if (auth()->user() && in_array(auth()->id(), [40])) {
+        //         \Debugbar::enable();
+        //     }
+        //     else {
+        //         \Debugbar::disable();
+        //     }
+        // }else{
+        //     if(!empty($request->header('x-forwarded-for'))){
+        //         $ip = $request->header('x-forwarded-for');
+        //     }else{
+        //         $ip = $request->ip();
+        //     }
+        //     if (!in_array($ip, $whiteIps)) {
+        //         return response("your ip address ( $ip ) is not valid.")->header('Content-Type', 'text/plain');
+        //         // return redirect()->to('https://google.com');
+        //     }else{
+        //         if (auth()->user() && in_array(auth()->id(), [40])) {
+        //             \Debugbar::enable();
+        //         }
+        //         else {
+        //             \Debugbar::disable();
+        //         }
+        //     }
+        // }
         return $next($request);
     }
 }
